812197ef2903dee36a3c4a237f6607d07a8a3e5d
WildanR19 <wildanrozaqi2000@gmail.com>
2022-10-03 15:41:51 +0700

    #changelog - add navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import 'assets/scss/style.scss'
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
   return (
     <div className="App">
-      Hello world
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/parts/IconText.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from "../elements/Button";
+
+const IconText = () => {
+  return (
+    <>
+      <Button className="navbar-brand brand-text-icon" href="" type="link">
+        Stay<span className="text-secondary">cation.</span>
+      </Button>
+    </>
+  );
+};
+
+export default IconText;
